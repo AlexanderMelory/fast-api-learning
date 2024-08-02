22f87042bff119516ea8c122906d460294e385e8
Alexander Scherbakov <webmasterdracarys@gmail.com>
2024-08-02 18:13:51 +0300

    create base business logic and first migration by Alembic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pydantic.fields import ModelField
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 import settings
@@ -24,12 +25,13 @@ class User(Base):
     """
     User model
     """
+
     __tablename__ = 'user'
 
     user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     surname = Column(String, nullable=False)
-    email = Column(EmailStr, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
     is_active = Column(Boolean, default=True)
 
 
@@ -41,8 +43,77 @@ class UserDAL:
     def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
-    async def create_user(self, name: str, surname: str, email: str) -> User:
+    async def create_user(self, name: str, surname: str, email: EmailStr) -> User:
         new_user = User(name=name, surname=surname, email=email)
         self.db_session.add(new_user)
         await self.db_session.flush()
         return new_user
+
+
+LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z]+$")
+
+
+class TunedModel(BaseModel):
+    """
+    Abstract Model
+    """
+
+    class Config:
+        """
+        Convert even non-dict objs to json
+        """
+
+        orm_mode = True
+
+
+class ShowUser(TunedModel):
+    user_id = uuid.UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+
+class UserCreate(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+
+    @validator('name', 'surname')
+    def validate_name(cls, value: str, field: ModelField) -> None:
+        print(f'{value = }')
+        print(f'{field = }')
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(status_code=422, detail=f'{field.name} should contains only letters')
+
+
+app = FastAPI(title='Learning FastAPI')
+user_router = APIRouter()
+
+
+async def _create_user(body: UserCreate) -> ShowUser:
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(name=body.name, surname=body.surname, email=body.email)
+            return ShowUser(
+                user_id=user.user_id,
+                name=user.name,
+                surname=user.surname,
+                email=EmailStr(user.email),
+                is_active=user.is_active,
+            )
+
+
+@user_router.post('/', response_model=ShowUser)
+async def create_user(body: UserCreate) -> ShowUser:
+    return await _create_user(body)
+
+
+main_api_router = APIRouter()
+main_api_router.include_router(user_router, prefix='/user', tags=['user'])
+app.include_router(main_api_router)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
