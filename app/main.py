c81d9245a02763593543a980fd10c5a7ecc5e93d
Alexander Scherbakov <webmasterdracarys@gmail.com>
2024-10-17 12:29:07 +0300

    create simple main module

--- app/main.py
+++ app/main.py
@@ -1,9 +1,45 @@
-from fastapi import FastAPI
+from datetime import date
+from typing import Optional
 
+from fastapi import FastAPI, Query, Depends
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/hotels")
-def get_hotels():
-    return "Отель Бридж Резорт 5 звезд"
+class HotelsSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+@app.get('/hotels')
+def get_hotels(search_args: HotelsSearchArgs = Depends()):
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post('/bookings')
+def add_booking(booking: SBooking):
+    pass
