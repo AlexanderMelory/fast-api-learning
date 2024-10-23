bda584b70105f075d53798b2e9faee4f2fdf4d84
Alexander Scherbakov <webmasterdracarys@gmail.com>
2024-10-23 13:10:53 +0300

    update settings.py code-style

--- app/settings.py
+++ app/settings.py
@@ -6,11 +6,11 @@ import dotenv
 dotenv.load_dotenv()
 
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_USER = os.getenv('DB_USER')
+DB_NAME = os.getenv('DB_NAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
